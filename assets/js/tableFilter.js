868d7d591e6a0b1db297ecd6bbf7d4d6e4dafbc7
DChrispim <brenoadsdc@gmail.com>
2024-04-17 12:42:27 -0300

    Organization

--- assets/js/tableFilter.js
+++ assets/js/tableFilter.js
@@ -1,53 +1,52 @@
-let mathElements = document.querySelectorAll(".math");
-let machineLearningElements = document.querySelectorAll(".machile-learning");
-let tableElements = document.getElementsByTagName("tr")
-let fixedToggleElements = document.querySelectorAll(".fixedToggle")
+// let mathElements = document.querySelectorAll(".math");
+// let machineLearningElements = document.querySelectorAll(".machile-learning");
 
+// Table elements
+let tableElements = document.getElementsByTagName("tr");
+let fixedToggleElements = document.querySelectorAll(".fixedToggle");
 
-let ActiveTags = ["math", "python", "machineLearning"]
+// Tags list
+let ActiveTags = ["coding", "frontEnd", "dataScience", "others"];
 
+// Toggle class in ActiveTags list
 function toggleClass(tagName) {
-    if (ActiveTags.includes(tagName)) {
-        let index = ActiveTags.indexOf(tagName);
-        ActiveTags.splice(index, 1);
-    } else {
-        ActiveTags.push(tagName)
-    }
+  if (ActiveTags.includes(tagName)) {
+    let index = ActiveTags.indexOf(tagName);
+    ActiveTags.splice(index, 1);
+  } else {
+    ActiveTags.push(tagName);
+  }
 }
 
-function hasActiveTag (tag) {
-    return ActiveTags.some(tag)
-}
-
+// Check table elements for elements with tag in ActiveTags list
 function check() {
-    for (let i=1; i < tableElements.length; i++) {
-        if (ActiveTags.length == 0) {
-            tableElements[i].style.display = "none";
+  for (let i = 1; i < tableElements.length; i++) {
+    if (ActiveTags.length == 0) {
+      tableElements[i].style.display = "none";
+    } else {
+      for (let j = 0; j < ActiveTags.length; j++) {
+        let classArray = Array.from(tableElements[i].classList);
+        if (classArray.includes(ActiveTags[j])) {
+          tableElements[i].style.display = "table-row";
+          break;
         } else {
-            for (let j=0; j<ActiveTags.length; j++) {
-                let classArray = Array.from(tableElements[i].classList);
-                if (classArray.includes(ActiveTags[j])) {
-                    tableElements[i].style.display = "table-row";
-                    break;
-                } else {
-                    tableElements[i].style.display = "none";
-                }
-            }
+          tableElements[i].style.display = "none";
         }
+      }
     }
+  }
 }
 
-
-
-for (let i=0; i<fixedToggleElements.length; i++) {
-    fixedToggleElements[i].addEventListener("click",function (event)  {
-        toggleClass(fixedToggleElements[i].id);
-        check();
-        event.preventDefault();
-        if (fixedToggleElements[i].classList.contains("active")) {
-            fixedToggleElements[i].classList.remove("active");
-        } else {
-            fixedToggleElements[i].classList.add("active");
-        };
-    })
+// Add evento to filter buttons
+for (let i = 0; i < fixedToggleElements.length; i++) {
+  fixedToggleElements[i].addEventListener("click", function (event) {
+    toggleClass(fixedToggleElements[i].id);
+    check();
+    event.preventDefault();
+    if (fixedToggleElements[i].classList.contains("primary")) {
+      fixedToggleElements[i].classList.remove("primary");
+    } else {
+      fixedToggleElements[i].classList.add("primary");
+    }
+  });
 }
